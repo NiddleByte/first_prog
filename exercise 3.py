9d7faabf7a86e33a40fe26d0ae8b57a1466fd396
NiddleByte <asepsayyad007@gmail.com>
2020-06-06 15:58:17 +0530

    new

--- exercise 3.py
+++ exercise 3.py
@@ -1,13 +1,27 @@
-n = int(input("Enter No of rows"))
-b = int(input("Enter 1 or 0"))
+no_rows = int(input("Enter No of rows"))
+tmp = int(input("Enter 1 or 0"))
+bool = bool(tmp)
+# if b == 0:
+#     temp = n
+#     while temp > 0:
+#         print(temp * "*")
+#         temp = temp - 1
+# else:
+#     temp = 0
+#     while temp < n:
+#         temp = temp + 1
+#         print(temp * "*")
+#
+#
 
-if b == 0:
-    temp = n
-    while temp > 0:
-        print(temp * "*")
-        temp = temp - 1
-else:
-    temp = 0
-    while temp < n:
-        temp = temp + 1
-        print(temp * "*")
+
+if bool == True:
+    for i in range ( 1 , no_rows+1):
+        for j in range (1 ,i+1):
+            print("*",end=" ")
+        print()
+elif bool == False:
+    for i in range(no_rows,0,-1):
+        for j in range (1,i+1):
+            print("*",end=" ")
+        print()
