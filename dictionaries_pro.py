53a1299210def7a0b898f66198a46c78108f8e05
NiddleByte <asepsayyad007@gmail.com>
2020-06-11 11:28:04 +0530

    dictionaries programs added

--- dictionaries_pro.py
+++ dictionaries_pro.py
@@ -1,17 +1,17 @@
-dict1 = {
-    'name' : 'Asep',
-    'age' : 24,
-    'fav_movie': 'coco',
-    'fav_song': 'sorry'
-}
-print(dict1)
-
-# dict1.pop('name')
-dict1.popitem()
+# dict1 = {
+#     'name' : 'Asep',
+#     'age' : 24,
+#     'fav_movie': 'coco',
+#     'fav_song': 'sorry'
+# }
 # print(dict1)
-print(dict1)
-print(type(dict1))
-print(dict1)
+#
+# # dict1.pop('name')
+# dict1.popitem()
+# # print(dict1)
+# print(dict1)
+# print(type(dict1))
+# print(dict1)
 #
 # if 'Asep' in dict1.values():
 #     print("present")
@@ -23,3 +23,34 @@ print(dict1)
 #
 # for i, j in dict1.items():
 #     print(f"{i} :: {j}")
+
+
+# d = dict1.fromkeys(['name','age','fav_movie'],'unknown')
+# print(d)
+#
+# d = dict1.fromkeys("abcde",'uknown')
+# print(d) #{'a': 'unknown', 'b': 'unknown', 'c': 'unknown'}
+
+
+# =======================
+# ===== Get() Method========
+
+# print(dict1['name'])
+
+# print(dict1.get('names')) ## none     #names is not in dictionary
+# print(dict1.get('name'))  ## asep     #name is present
+
+
+# ========== Cube Finder ============
+
+# def cube_find(n):
+#     dict1={}
+#     for i in range(1,n+1):
+#         dict1[i]=i**3
+#         print(i ,":", dict1[i])
+#     return dict1
+# # n = int(input("Enter your number for cube"))
+# print(cube_find(4))
+# # cube_find(n)
+
+# ======== Word Counter =============
